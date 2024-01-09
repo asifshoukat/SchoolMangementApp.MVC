aab60382093e04537374e38a62967737e12b5298
Asif Shoukat <asifshoukat@gmail.com>
2024-01-09 12:58:50 +1100

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolMangementApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
